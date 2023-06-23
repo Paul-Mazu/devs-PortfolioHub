51a1a4380c6dc6f30759c92744ecbc731b7037e7
Pawel Mazurkiewicz <mailbox.mazu@gmail.com>
2023-06-23 14:50:35 +0200

    add api docs

--- src/backend/app/core/urls.py
+++ src/backend/app/core/urls.py
@@ -2,8 +2,15 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls", namespace="user")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="api-schema"),
+        name="api-docs",
+    ),
 ]
