5c632b85b4af28ed38b9df1730026ebf0cd9da34
chris01 <chris_odum@yahoo.com>
2023-06-23 15:32:16 +0200

    Create app, url project path and models

--- src/backend/app/core/urls.py
+++ src/backend/app/core/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
     path("api/user/", include("user.urls", namespace="user")),
+    path("api/project/", include("project.urls", namespace="project")),
 ]
--- src/backend/app/project/models.py
+++ src/backend/app/project/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):    
+    name = models.CharField(max_length=25,primary_key=True)    
+    created = models.DateTimeField(auto_now_add=True)    
+    
+    def __str__(self):        
+        return f"#{self.name} "
+
+class Project(models.Model):
+    """
+    Photo of project
+    Project name
+    Author full  name
+    Short description 200signs
+    Long Description - text
+    max 5 tags - tech stack
+    Link github
+    link live demo
+    """
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='')
+    short_desc= models.CharField(max_length=255)
+    long_desc = models.TextField()
+    tech_stack = models.OneToMany("max 5")
+    github_link=""
+    demo_link=""
+
