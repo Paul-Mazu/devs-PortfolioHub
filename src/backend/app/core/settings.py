c7cfdddf644a40896cd7384918d62cecdbc7d8c8
Pawel Mazurkiewicz <mailbox.mazu@gmail.com>
2023-06-23 12:18:42 +0200

    create user app, create models, serializers, routing, tests, first apis

--- src/backend/app/core/settings.py
+++ src/backend/app/core/settings.py
@@ -39,8 +39,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # my
     "rest_framework",
     "rest_framework.authtoken",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -119,10 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = "/files/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# use custom User from user app
+AUTH_USER_MODEL = "user.User"
