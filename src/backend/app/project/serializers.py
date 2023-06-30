245b1893f4d252a5ee7d972feb038c80f774a3ca
Pawel Mazurkiewicz <mailbox.mazu@gmail.com>
2023-06-30 14:19:07 +0200

    add create and update tags in project, add test hints

--- src/backend/app/project/serializers.py
+++ src/backend/app/project/serializers.py
@@ -24,11 +24,34 @@ class ProjectSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ["id", "author"]
 
+    def _get_or_create_tags(self, tags, project):
+        """Handle getting or creating tags as needed"""
+        for tag in tags:
+            tag_obj, _ = Tag.objects.get_or_create(
+                **tag,
+            )
+            project.tags.add(tag_obj)
+
     def create(self, validated_data):
         """Create and return project."""
         tags = validated_data.pop("tags", [])
         project = Project.objects.create(**validated_data)
-        for tag in tags:
-            tag_obj, _ = Tag.objects.get_or_create(**tag)
-            project.tags.add(tag_obj)
+        self._get_or_create_tags(tags, project)
         return project
+
+    def update(self, instance, validated_data):
+        """Update and return data"""
+        tags = validated_data.pop("tags", None)
+        project = super().update(instance, validated_data)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+        return project
+
+"""
+Please test if:
+- Test if create new project with tags successful. example: user\tests\test_user_api.py line 156
+- Test creating project with existing tags. example: user\tests\test_user_api.py line 174
+- Test if possible to update tags field. example: user\tests\test_user_api.py line 249
+- Test if reassigning existing tags while update successful. example: user\tests\test_user_api.py line 268
+"""
