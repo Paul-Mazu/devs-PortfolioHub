a7eca5d3fc85f58647c569b3782d5173360ea5d1
alj-b <aljoscha_b@icloud.com>
2023-07-07 16:18:21 +0200

    Order imports in App.js

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,11 +1,10 @@
-
 import './App.css';
 import Header from "./components/Header/Header";
 import LandingPage from "./components/LandingPage/LandingPage";
 import ProjectList from "./components/ProjectList/ProjectList";
+import DeveloperList from './components/DeveloperList/DeveloperList';
 import MyProfile from "./components/MyProfile/MyProfile";
 import Footer from "./components/Footer/Footer";
-import DeveloperList from './components/DeveloperList/DeveloperList';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
