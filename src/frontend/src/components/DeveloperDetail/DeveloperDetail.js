3d59c51de2ebf33316cb8294bd3d1d1cab217e1b
alj-b <aljoscha_b@icloud.com>
2023-07-07 16:02:00 +0200

    Create basic DeveloperDetail component

---
+++ src/frontend/src/components/DeveloperDetail/DeveloperDetail.js
@@ -0,0 +1,20 @@
+import "./DeveloperDetail.css";
+
+// The developer detail page should automatically GET /api/user/me/ and display its data as a developer profile component.
+
+export default function DeveloperDetail({developer, isUser}) {
+    if (isUser) {
+        return (
+            <div>
+                <p className="description">Welcome to your profile</p>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div>
+                <p className="description">Welcome to {developer} profile</p>
+            </div>
+        );
+    }
+}
