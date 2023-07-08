77b002e304137817410eac176ab529ef9a135734
alj-b <aljoscha_b@icloud.com>
2023-07-09 00:30:25 +0200

    Create new api folder in src to hold functions interacting with backend APIs

---
+++ src/frontend/src/api/developers.api.js
@@ -0,0 +1,3 @@
+import axios from "axios";
+
+// a file to bundle and export the various developer APIs (find all, filter, find one by ID, find active user)
---
+++ src/frontend/src/api/projects.api.js
@@ -0,0 +1,3 @@
+import axios from "axios";
+
+// a file to bundle and export the various project APIs (find all, filter, find one by ID)
