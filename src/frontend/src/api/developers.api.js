61a27e29f9380e72640cbd1fef7639292f0f2830
alj-b <aljoscha_b@icloud.com>
2023-07-09 00:46:27 +0200

    Prepare basic APIs

--- src/frontend/src/api/developers.api.js
+++ src/frontend/src/api/developers.api.js
@@ -1,3 +1,47 @@
 import axios from "axios";
 
 // a file to bundle and export the various developer APIs (find all, filter, find one by ID, find active user)
+
+const BASE_URL = "http://localhost:8000"
+
+export async function getAllDevelopers () {
+    try {        
+        let foundDevelopers = await axios.get(BASE_URL + "api/user/users/", { withCredentials: true });
+        return foundDevelopers;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// the filters parameter is simply a placeholder for now for the actual filters we want to use depending on the backend API provided
+
+export async function getFilteredDevelopers (filters) {
+    try {        
+        let foundDevelopers = await axios.get(BASE_URL + "api/user/users/", {params: {filters: filters}}, { withCredentials: true });
+        return foundDevelopers;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// the actual implementation depends on the backend API - i.e. id in params or in url? /api/user/users/{id}/
+
+export async function getDeveloperById (id) {
+    try {        
+        let foundDeveloper = await axios.get(BASE_URL + "api/user/users/" + id, { withCredentials: true });
+        return foundDeveloper;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export async function getCurrentDeveloper () {
+    try {        
+        let foundDeveloper = await axios.get(BASE_URL + "/api/user/me/", { withCredentials: true });
+        return foundDeveloper;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
