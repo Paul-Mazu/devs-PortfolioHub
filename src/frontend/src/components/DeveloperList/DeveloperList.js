9948107092120d53bb40c5768d5761670c36729d
alj-b <aljoscha_b@icloud.com>
2023-07-10 16:58:33 +0200

    Start work on displaying API results. For now constantly firing API requests.

--- src/frontend/src/components/DeveloperList/DeveloperList.js
+++ src/frontend/src/components/DeveloperList/DeveloperList.js
@@ -1,7 +1,8 @@
 import "./DeveloperList.css";
 import axios from "axios";
+import React, { useEffect, useState } from 'react';
 import { getAllDevelopers } from "../../api/developers.api";
-import ImgMediaCard from "../Card/Card";
+import DeveloperCard from "../Card/Card";
 
 
 // The developer list page should contain a gallery of card components that each show and link to a developer.
@@ -20,12 +21,59 @@ export default function DeveloperList() {
   //   }
   // };
 
-  getAllDevelopers()
+  const [developers, setDevelopers] = useState([]);
+
+  // const handleChange = async (terms) => {
+  //   terms.length ? setLoading(true) : setLoading(false);
+  //   const result = await getRemedyRecommendation(terms);
+  //   const remediesToShow = result ? result.slice(0, 10) : [];
+  //   console.log("Selected symptom(s)", terms);
+  //   if (!terms.length) {
+  //     setRemedies([]);
+  //     console.log("no data found");
+  //   } else {
+  //     setRemedies(remediesToShow);
+  //     setLoading(false);
+  //   }
+  //   console.log("Matching recommendations:", remediesToShow);
+  // };
+
+  // const handleChange = async () => {
+  //   const result = await getAllDevelopers();
+  //   setDevelopers(result);
+  // };
+
+  // useEffect(() => {
+  //   const result = getAllDevelopers();
+  //   console.log(result);
+  //   console.log(result.state);
+  //   setDevelopers(result.data);
+  // }, [])
+  
+  // useEffect(() => {
+  //   getAllDevelopers().then((value) => {
+  //     setDevelopers(value.data);
+  //   })
+  // }, [])
+
+  // this sends a million requests per second
+  
+  const result = getAllDevelopers()
+  result.then((value) => {
+    setDevelopers(value.data);
+    console.log(value);
+  }).catch(err => {
+    console.log(err);
+  });
+
   return (
     <div className="main">
       <p className="description">Discover the best developers</p>
-      
-      <ImgMediaCard />
+      {developers.map((developer) =>
+        <DeveloperCard
+            developer={developer}
+        />)
+      }
     </div>
   );
 }
