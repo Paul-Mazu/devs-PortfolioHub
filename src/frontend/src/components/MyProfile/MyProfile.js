1977e0091dc2bf6e1722e6bdca0e106d5bbb89fa
alj-b <aljoscha_b@icloud.com>
2023-07-14 22:27:39 +0200

    Create MyProfile component that uses stored userToken to call the user/me api

--- src/frontend/src/components/MyProfile/MyProfile.js
+++ src/frontend/src/components/MyProfile/MyProfile.js
@@ -1,45 +1,58 @@
 import "./MyProfile.css";
 import DeveloperDetail from "../DeveloperDetail/DeveloperDetail";
+import { useEffect, useState } from 'react';
+import { getCurrentDeveloper } from "../../api/developers.api";
+import { getToken } from "../../helpers/helpers.js"
 
 // The my profile page should automatically GET /api/user/me/ and display its data as a developer profile component.
 
-const dummyDeveloper = {
-    "id": 0,
-    "email": "user@example.com",
-    "profile_image": "string",
-    "name": "string",
-    "short_desc": "string",
-    "tags": [
-      {
-        "name": "string"
-      }
-    ],
-    "bio": "string",
-    "title": "string",
-    "address": "string",
-    "working_at": "string",
-    "status_open_to_work": true,
-    "github_link": "string",
-    "linkedin_link": "string",
-    "website_link": "string",
-    "xing_link": "string",
-    "whatsapp": "string",
-    "messenger": "string"
-  }
+// {
+//   "id": 0,
+//   "email": "user@example.com",
+//   "profile_image": "string",
+//   "name": "string",
+//   "short_desc": "string",
+//   "tags": [
+//     {
+//       "name": "string"
+//     }
+//   ],
+//   "bio": "string",
+//   "title": "string",
+//   "address": "string",
+//   "working_at": "string",
+//   "status_open_to_work": true,
+//   "github_link": "string",
+//   "linkedin_link": "string",
+//   "website_link": "string",
+//   "xing_link": "string",
+//   "whatsapp": "string",
+//   "messenger": "string"
+// }
 
 export default function MyProfile() {
-   return (
-        <div className="main">
-            <h2>Example for isUser=false (i.e. the profile of somebody else, but not the current user). Obviously, the route should be different:</h2>
+  const [developer, setDeveloper] = useState([]);
+
+  const userToken = getToken();
+  console.log(userToken);
+
+  useEffect(() => {
+    getCurrentDeveloper(userToken)
+    .then((response) => setDeveloper(response.data))
+    .catch(e => setDeveloper([]));
+  }, []);
+
+  return (
+    <div className="main">
+      {/* <h2>Example for isUser=false (i.e. the profile of somebody else, but not the current user). Obviously, the route should be different:</h2>
             <DeveloperDetail 
                 isUser={false}
                 developer={dummyDeveloper}
-            />
-            <h2>Example for isUser=true (i.e. the profile of the current user):</h2>
-            <DeveloperDetail 
-                isUser={true}
-                developer={dummyDeveloper}
-            />
-        </div>
+            /> */}
+      <DeveloperDetail
+        isUser={true}
+        developer={developer}
+      />
+    </div>
   );
 }
