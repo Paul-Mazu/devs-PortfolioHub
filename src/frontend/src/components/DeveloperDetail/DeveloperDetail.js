9f54efc3e2049978e475b9a6ca51db527b2c4847
alj-b <aljoscha_b@icloud.com>
2023-07-17 20:38:45 +0200

    Add conditionals within JSX return

--- src/frontend/src/components/DeveloperDetail/DeveloperDetail.js
+++ src/frontend/src/components/DeveloperDetail/DeveloperDetail.js
@@ -2,22 +2,27 @@ import "./DeveloperDetail.css";
 
 // The developer detail page should automatically GET /api/user/me/ and display its data as a developer profile component.
 
-export default function DeveloperDetail({developer, isUser}) {
-    if (isUser) {
-        return (
-            <div>
+export default function DeveloperDetail({ developer, isUser }) {
+
+    // const checkUserStatus = (isUser) => {
+    //     if (isUser) {
+    //         return `Welcome to your profile, ${developer.name}`
+    //     } else {
+    //         return `Welcome to ${developer.name}'s profile`
+    //     }
+    // };
+
+    return (
+        <div>
+            {isUser === true &&
                 <p className="description">Welcome to your profile, {developer.name}</p>
-                <h2>{developer.name}</h2>
-                <h2>{developer.email}</h2>
-                <h2>{developer.bio}</h2>
-            </div>
-        );
-    }
-    else {
-        return (
-            <div>
+            }
+            {isUser === false &&
                 <p className="description">Welcome to {developer.name}'s profile</p>
-            </div>
-        );
-    }
+            }
+            <h2>{developer.name}</h2>
+            <h2>{developer.email}</h2>
+            <h2>{developer.bio}</h2>
+        </div>
+    );
 }
