eef5a61edc7e88f957fead865fba7d395d38a4a7
alj-b <aljoscha_b@icloud.com>
2023-07-19 10:07:25 +0200

    Correct names

--- src/frontend/src/components/Footer/Footer.js
+++ src/frontend/src/components/Footer/Footer.js
@@ -4,8 +4,8 @@ export default function Footer () {
     return (
         <footer>
 Coded by <a href="">Pawel Mazurkiewicz</a> 
-            <a href="">Christofer Odum</a>
-            <a href="">Emre Sandicjoglu</a>
+            <a href="">Christopher Odum</a>
+            <a href="">Emre Sandikcioglu</a>
             <a href="">Aljoscha Beiers</a>
             <a href="">Maryna Solovarova</a>
 
