c6a36e568f6233fd20954845de3b9f6a61d729d7
alj-b <aljoscha_b@icloud.com>
2023-07-21 12:10:40 +0200

    Adjust class name to style error html elements only without affecting other components

--- src/frontend/src/components/ErrorMessage/ErrorMessage.js
+++ src/frontend/src/components/ErrorMessage/ErrorMessage.js
@@ -5,8 +5,8 @@ export default function ErrorMessage({e}) {
     console.log(e);
   
     return (
-      <div className="main">
-        <p>You encountered an error {e.code}</p>
+      <div className="error">
+        <p>You encountered an error {e.code}:</p>
         <p>{e.message}</p>
       </div>
     );
