f0c74f1e3fdd83c8ee6998e5ecef27b236de17ff
alj-b <aljoscha_b@icloud.com>
2023-07-29 00:11:32 +0200

    Create a getFilteredProjects API call for search function

--- src/frontend/src/api/projects.api.js
+++ src/frontend/src/api/projects.api.js
@@ -1,3 +1,16 @@
 import axios from "axios";
 
 // a file to bundle and export the various project APIs (find all, filter, find one by ID)
+
+// filtered project search
+
+const BASE_URL = "http://35.204.79.162/"
+
+export async function getFilteredProjects (name, tags, author) {
+    try {        
+        let foundProjects = await axios.get(BASE_URL + "api/user/users/", {params: {name: name, tags: tags, author: author}}, { withCredentials: true });
+        return foundProjects;
+    } catch (error) {
+        console.log(error);
+    }
+};
