99e1dec8d5b942a329b98560e4391beacf0710e8
alj-b <aljoscha_b@icloud.com>
2023-07-31 01:46:12 +0200

    Refine handleInputChange function and create handleSubmit function

--- src/frontend/src/components/MyProfileEditForm/MyProfileEditForm.js
+++ src/frontend/src/components/MyProfileEditForm/MyProfileEditForm.js
@@ -6,8 +6,21 @@ import { getToken } from "../../helpers/helpers.js";
 export default function RegistrationForm() {
     const [activeUser, setActiveUser] = useState(false);
     const [loading, setLoading] = useState(true);
-    const [name, setName] = useState("");
-    const [data, setData] = useState("");
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        profile_image: "",
+        short_desc: "",
+        tags: [],
+        bio: "",
+        title: "",
+        working_at: "",
+        address: "",
+        status_open_to_work: false,
+        github_link: "",
+        linkedin_link: "",
+        website_link: "",
+    });
   
     const userToken = getToken();
   
@@ -21,12 +34,13 @@ export default function RegistrationForm() {
 
     const handleInputChange = (e) => {
         const { id, value } = e.target;
-        if (id === "name") {
-            setName(value);
-        }
-        if (id === "data") {
-            setData(value);
-        }
+        setData({id: value});
+    }
+
+    const handleSubmit = (e) => {
+        // package modified fields into a single data object to submit to edit form
+        const data = e.target
+        userEdit(userToken, data)
     }
 
     // should also contain various validation functions for email, password and the form itself
@@ -34,22 +48,26 @@ export default function RegistrationForm() {
     return (
         <div className="registration-container">
             <div className="form-card">
-                <h2>Registration Form</h2>
-                <h3>Register to create a profile and curate your own portfolio</h3>
+                <h2>Edit Form</h2>
+                <h3>Edit your profile data</h3>
 
                 <div className="form-inputs">
                     <div className="input-field">
                         <label className="form-label" htmlFor=""></label>
-                        <input className="input" type="text" id="name" value={name} onChange={(e) => handleInputChange(e)} placeholder="Name" required/>
+                        <input className="input" type="text" id="name" value={data.name} onChange={(e) => handleInputChange(e)} placeholder={activeUser.name}/>
                     </div>
                     <div className="input-field">
                         <label className="form-label" htmlFor=""></label>
-                        <input className="input" type="email" id="email" value={data} onChange={(e) => handleInputChange(e)} placeholder="" required/>
+                        <input className="input" type="email" id="email" value={data.email} onChange={(e) => handleInputChange(e)} placeholder={activeUser.email}/>
+                    </div>
+                    <div className="input-field">
+                        <label className="form-label" htmlFor=""></label>
+                        <input className="input" type="text" id="profile_image" value={data.profile_image} onChange={(e) => handleInputChange(e)} placeholder={activeUser.profile_image}/>
                     </div>
                 </div>
 
                 {/* <Link to="/profile"> */}
-                <button onClick={() => userEdit(userToken, data)} type="submit" className='form-button'>Register</button>
+                <button onClick={(e) => handleSubmit(e)} type="submit" className='form-button'>Submit edits</button>
                 {/* </Link> */}
             </div>
         </div>
