ba66006df9921c58f7bd7a0843871df9411abf03
alj-b <aljoscha_b@icloud.com>
2023-08-04 00:34:00 +0200

    Create one time function returning an array of tag objects to populate DB with

--- src/frontend/src/helpers/helpers.js
+++ src/frontend/src/helpers/helpers.js
@@ -11,3 +11,11 @@ export const getToken = () => {
     const userToken = JSON.parse(tokenString)
     return userToken
 };
+
+// one time: populate DB with tags
+let tags = ["Angular","Python","MongoDB","Xcode","React Native","Swift","Node.js","IntelliJ","Rust","Flutter","ASP.NET","Apache Spark","PHPStorm","Ruby","Xamarin","PHP","Assembly","PowerShell","Couchbase","Cassandra","SQLite","Sublime Text","Pandas","C++","Clojure","TextMate","MySQL","Objective-C","Eclipse","Firebase",".NET","Cordova","Oracle","CSS","Django","R","Kotlin","Erlang","Visual Studio Code","F#","Shell","DynamoDB","React.js","SQL","Android Studio","Chef","Komodo","RubyMine","Puppet","Angular.js","Microsoft SQL Server",".NET Core","Notepad++","PyTorch","TensorFlow","Redis","Unreal Engine","Elasticsearch","MariaDB","Light Table","CryEngine","Atom","PyCharm","Ruby on Rails","HTML"," Jupyter","Vue.js","Torch","Other(s)","Express","Vim","Hadoop","Dart","jQuery","Elixir","Visual Studio","Flask","Emacs","RStudio","Drupal","WebAssembly","Ansible","C#","Zend","Bash","JavaScript","Scala","Java","VBA","NetBeans","Coda","C","IPython ","Unity 3D","Spring","Go","Laravel","PostgreSQL","TypeScript","Verse"]
+export const populateTags = () => {
+    let tagArray = [];
+    tags.forEach((tag) => tagArray.push(new Object({name: tag})));
+    return tagArray
+};
