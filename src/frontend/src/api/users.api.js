aac160241dcde6b976978cbd4ba41a9530ca69a5
alj-b <aljoscha_b@icloud.com>
2023-08-03 14:21:39 +0200

    Remove extra console.lofs

--- src/frontend/src/api/users.api.js
+++ src/frontend/src/api/users.api.js
@@ -6,12 +6,6 @@ import { setToken } from "../helpers/helpers.js";
 // const BASE_URL = "http://localhost:8000/"
 const BASE_URL = "http://35.204.79.162/"
 
-// {
-//     "email": "user@example.com",
-//     "password": "string",
-//     "name": "string",
-//   }
-
 export async function userRegistration (name, email, password) {
     try {
         await axios({
@@ -29,63 +23,17 @@ export async function userRegistration (name, email, password) {
     }
     catch (err) {
         console.log(err.message);
-        // handleErrorMessage();
-
-        // const registerData = await registerResponse;
-        //need to return response as below
-        // return registerData.json();
     }
 }
-export async function userEdit (userToken, data) {
-    
-    // with this, I can successfully return a file or blob, however neither is accepted by the API
-
-    async function createFile(imgUrl){
-        let img = new Image();
-        img.crossOrigin = "anonymous";
-        img.src = imgUrl;
-        let response = await fetch(imgUrl);
-        let data = await response.blob();
-        let metadata = {
-        type: 'image/jpeg'
-        };
-        let file = new File([img], "test.jpg", metadata);
-        let fileReader = new FileReader();
-        fileReader.onload = function (evt) {success(evt.target.result)};
-        fileReader.readAsText(file);
-        let binaryString = fileReader.result;
-        console.log("createFile binary string");
-        console.log(binaryString);
-        console.log(fileReader);
-        return fileReader;
-    }
-
-    function success (content) {
-        console.log("Success"); 
-        let string = JSON.stringify(content)
-        console.log(string); 
-        data.profile_image = string;
-    }
-      
-    
+export async function userEdit (userToken, data) {   
 
     // data is an object with key-value pairs
     // extract keys and values to pass into axios data parameter dynamically
 
     Object.keys(data).forEach((k) => data[k] === undefined && delete data[k]);
 
-    if (data.profile_image !== undefined) {
-        let binaryString = await createFile(data.profile_image);
-        console.log("binary string");
-        console.log(binaryString);
-        // data.profile_image = binaryString;
-    }
-    console.log("data");
-    console.log(data);
-
     try {
         console.log("Trying...");
-        console.log(data);
         await axios({
             method: 'patch',
             withCredentials: true,
@@ -96,17 +44,16 @@ export async function userEdit (userToken, data) {
             },
             data: data
         });
-        // window.location = "/profile";
+        window.location = "/profile/edit";
         console.log(`User updated successfully`);
     }
     catch (err) {
         console.log("Catching...");
-        console.log(data);
         console.log(err.message);
     }
 }
 
-export async function userLogIn (email, password) {
+export async function userLogIn (email, password, edit = false) {
     console.log(email);
 
     try {
