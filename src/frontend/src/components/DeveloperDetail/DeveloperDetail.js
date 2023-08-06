15e6060e25815de2fc254d5386bc3cedb84515b0
alj-b <aljoscha_b@icloud.com>
2023-08-06 21:29:02 +0200

    Add conditional rendering to Socials and add appropriate attributes

--- src/frontend/src/components/DeveloperDetail/DeveloperDetail.js
+++ src/frontend/src/components/DeveloperDetail/DeveloperDetail.js
@@ -30,13 +30,13 @@ import placeholderImage from "../../images/Drawing.png";
 
 export default function DeveloperDetail({ developer, isUser }) {
 
-    // const checkUserStatus = (isUser) => {
-    //     if (isUser) {
-    //         return `Welcome to your profile, ${developer.name}`
-    //     } else {
-    //         return `Welcome to ${developer.name}'s profile`
-    //     }
-    // };
+  // const checkUserStatus = (isUser) => {
+  //     if (isUser) {
+  //         return `Welcome to your profile, ${developer.name}`
+  //     } else {
+  //         return `Welcome to ${developer.name}'s profile`
+  //     }
+  // };
 
   const checkImageUrl = (image) => {
     if (image === null) {
@@ -52,33 +52,45 @@ export default function DeveloperDetail({ developer, isUser }) {
       return developer.title;
     }
   }
+
   const tags = developer.tags;
   const listTags = tags.map((d, idx) => <h3 className="tag" key={idx}>{d.name}</h3>);
 
-    return (
-        <div className="container-main">
-            {isUser === true &&
-                <div>
-                    <p className="description">Welcome to your profile, {developer.name}</p>
-                    <a href="/profile/edit">Edit your profile</a>
-                    <a href="/">Add a project</a>
-                </div>                
-            }
-            {isUser === false &&
-                <p className="description">Welcome to {developer.name}'s profile</p>
-            }
-            <img className={"profile-dev-pic " + developer.status_open_to_work} src={checkImageUrl(developer.profile_image)} alt="Developer profile"></img>
-            <h2>Name: {developer.name}</h2>
-            <h2>Email: {developer.email}</h2>
-            <h3>Position: {checkPosition(developer)}</h3>
-            <h3>Short Description: {developer.short_desc}</h3>
-            <h3>{listTags}</h3>
-            <p>Bio: {developer.bio}</p>
-            <p>Highlighted Projects: </p>
-            <a>See all Projects by {developer.name}</a>
-
-            {/* code for social media links */}
-            <p>Socials: </p>
+  return (
+    <div className="container-main">
+      {isUser === true &&
+        <div>
+          <p className="description">Welcome to your profile, {developer.name}</p>
+          <a href="/profile/edit">Edit your profile</a>
+          <a href="/">Add a project</a>
         </div>
-    );
+      }
+      {isUser === false &&
+        <p className="description">Welcome to {developer.name}'s profile</p>
+      }
+      <img className={"profile-dev-pic " + developer.status_open_to_work} src={checkImageUrl(developer.profile_image)} alt="Developer profile"></img>
+      <h2>Name: {developer.name}</h2>
+      <h2>Email: {developer.email}</h2>
+      <h3>Position: {checkPosition(developer)}</h3>
+      <h3>Short Description: {developer.short_desc}</h3>
+      <h3>{listTags}</h3>
+      <p>Bio: {developer.bio}</p>
+      <p>Highlighted Projects: </p>
+      <a>See all Projects by {developer.name}</a>
+      {(developer.github_link || developer.linkedin_link || developer.website_link) &&
+        <p>Socials: </p>
+      }
+      <ul>
+        {developer.github_link &&
+          <li>{developer.github_link}</li>
+        }
+        {developer.linkedin_link &&
+        <li>{developer.linkedin_link}</li>        
+        }
+        {developer.website_link &&
+        <li>{developer.website_link}</li>        
+        }
+      </ul>
+    </div>
+  );
 }
