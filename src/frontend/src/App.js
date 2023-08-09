ce7e545dca0c4bfb5dd44ac70a95aeb59b661bb6
alj-b <aljoscha_b@icloud.com>
2023-08-09 22:03:59 +0200

    Apply same logic to Footer

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -24,6 +24,7 @@ function App() {
               <>
                 <Navbar />
                 <Outlet />
+                <Footer />
               </>
             )}
           >
@@ -37,7 +38,6 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
-      <Footer />
     </div>
   );
 }
