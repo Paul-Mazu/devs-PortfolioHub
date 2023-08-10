b63ce813e0c589535310f658c560dffd66d8637d
alj-b <aljoscha_b@icloud.com>
2023-08-10 23:52:02 +0200

    Copy developer styling to project

--- src/frontend/src/components/Card/Card.js
+++ src/frontend/src/components/Card/Card.js
@@ -77,9 +77,6 @@ export function DeveloperCard({ developer }) {
   );
 }
 
-
-
-
 export function ProjectCard({ project }) {
   const checkImageUrl = (project_image) => {
     if (project_image === null) {
@@ -91,8 +88,7 @@ export function ProjectCard({ project }) {
   const projectName = project.name;
   const projectAuthor = project.author.name;
 
-
-  const tags = project.tags;
+  const tags = project.tags.slice(0, 5);
   const listTags = tags.map((d, idx) => (
     <h3 className="tag" key={idx}>
       {d.name}
@@ -102,24 +98,25 @@ export function ProjectCard({ project }) {
   return (
     // for now: developers/:id to make profiles accessible through URL and give option to bookmark them
     // <Link to={"/developers/" + slugify(developer.name)} state={{userId: developer.id}}>
-    <Link to={"/projects/" + project.id}>
-      <div className="card-main">
+    <div className="card-main">
+      <Link to={"/projects/" + project.id}>
         <div className="card-header">
           <img
-            className="card-dev-pic "
+            className="card-project-pic"
             src={checkImageUrl(project.project_image)}
             alt="Project Pic"
           ></img>
           <div className="card-header-titles">
-            <h2 className="card-title">{project.name}</h2>
-            <h3 className="card-title">{project.author.name}</h3>
-            <div className="tag-list">{listTags}</div>
+            <h2 className="card-title font-jost weight-semi-bold">{project.name}</h2>
+            <h3 className="card-title font-jost weight-normal">{project.author.name}</h3>
+            <div className="tag-list font-sen weight-semi-bold">{listTags}</div>
           </div>
         </div>
+        <hr className="card-break"></hr>
         <div className="card-body">
-          <p className="card-text">{project.short_desc}</p>
+          <p className="card-text font-sen weight-semi-bold">{project.short_desc}</p>
         </div>
-      </div>
-    </Link>
+      </Link >
+    </div>
   );
 }
